04a740fd686c6cbdc249557315f3a6bbc9075139
Azimbek <azimbekhakimov2@gmail.com>
2026-03-30 15:36:28 +0500

    Refactor channel environment variable assignments

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-SOURCE_CHANNEL = os.getenv("SOURCE_CHANNEL", "@malepeg")
-DESTINATION_CHANNEL = os.getenv("DESTINATION_CHANNEL", "@nazirovlix_blog")
+SOURCE_CHANNEL = os.getenv("SOURCE_CHANNEL")
+DESTINATION_CHANNEL = os.getenv("DESTINATION_CHANNEL")
 
 OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-5.4")
